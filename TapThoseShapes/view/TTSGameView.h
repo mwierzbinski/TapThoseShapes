8707ce372592aeb940e240f89d91e56d2721cdea
Michal Wierzbinski <michal.wierzbinski@gmail.com>
2015-05-10 12:10:32 +0200

    Added main game view.

---
+++ TapThoseShapes/view/TTSGameView.h
@@ -0,0 +1,15 @@
+//
+//  TTSGameView.h
+//  TapThoseShapes
+//
+//  Created by Michal Wierzbinski on 10/05/2015.
+//  Copyright (c) 2015 WheelReinvented. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTSGameView : UIView
+
+@property (retain, nonatomic) IBOutlet UIImageView *shapesField;
+
+@end
---
+++ TapThoseShapes/view/TTSGameView.m
@@ -0,0 +1,19 @@
+//
+//  TTSGameView.m
+//  TapThoseShapes
+//
+//  Created by Michal Wierzbinski on 10/05/2015.
+//  Copyright (c) 2015 WheelReinvented. All rights reserved.
+//
+
+#import "TTSGameView.h"
+
+@implementation TTSGameView
+
+
+
+- (void)dealloc {
+    [_shapesField release];
+    [super dealloc];
+}
+@end
