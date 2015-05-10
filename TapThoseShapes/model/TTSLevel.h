8b26f828f9faf797dcd14729bf62cc7847876eca
Michal Wierzbinski <michal.wierzbinski@gmail.com>
2015-05-10 11:51:43 +0200

    Added level model.

---
+++ TapThoseShapes/model/TTSLevel.h
@@ -0,0 +1,31 @@
+//
+//  TTSLevel.h
+//  TapThoseShapes
+//
+//  Created by Michal Wierzbinski on 09/05/2015.
+//  Copyright (c) 2015 WheelReinvented. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TTSShape.h"
+#import "TTSCircle.h"
+#import "TTSSquare.h"
+#import "TTSTriangle.h"
+
+// currently there is one level but
+// if there will be time we could change this to properties of the level
+// so the levels would differ
+
+static NSInteger kNoStartingShapes = 20; // number of Shapes you start with on the lvl
+static NSInteger kMinNodes = 10; // minimum number of Shapes at this lvl
+static NSInteger kMaxNodes = 200; // max number of Shapes at this lvl
+static NSInteger kTimeInterval = 1; // spawning time of Shapes
+
+@interface TTSLevel : NSObject
+
+@property (nonatomic, retain) NSArray *shapeList;
+
+- (void)initializeLevel; //should be called after we set level properties
+
+
+@end
---
+++ TapThoseShapes/model/TTSLevel.m
@@ -0,0 +1,79 @@
+//
+//  TTSLevel.m
+//  TapThoseShapes
+//
+//  Created by Michal Wierzbinski on 09/05/2015.
+//  Copyright (c) 2015 WheelReinvented. All rights reserved.
+//
+
+#import "TTSLevel.h"
+
+static float kMinShapeSize = 44; // we could make it small but that would be difficult to hit with a finger ;)
+
+@implementation TTSLevel
+
+#pragma mark - init/dealoc
+
+- (void)dealloc {
+    [_shapeList release];
+    [super dealloc];
+}
+
+- (void)initializeLevel {
+    
+    NSMutableArray *initialShapesArray = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i < kNoStartingShapes; i++) {
+        [initialShapesArray addObject:[self createRandomShape]];
+    }
+    
+    //making sure if were not adding empty array
+    self.shapeList = [initialShapesArray count] > 0 ? initialShapesArray : nil;
+    
+    [initialShapesArray release];
+}
+
+#pragma mark - creating shapes
+
+- (TTSShape *)createRandomShape {
+    
+    TTSShape *randomShape = nil;
+    
+    // randomize type
+    STShapeType shapeType = arc4random_uniform(3) + 1; // there are 3 types of shapes
+    
+    // randomize size
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    int maxSize = screenRect.size.width * .2; // there is no point in making the shapes too big.
+    float side = arc4random_uniform(maxSize) + kMinShapeSize;
+    
+    CGSize randomSize = CGSizeMake(side, side); // we want squares and circles, not rectangles and elipses.
+
+    switch (shapeType) {
+        case STShapeTypeCircle:
+            randomShape = [[TTSCircle alloc] initWithSize:randomSize];
+            break;
+        case STShapeTypeSquare:
+            randomShape = [[TTSSquare alloc] initWithSize:randomSize];
+            break;
+        case STShapeTypeTriangle:
+            randomShape = [[TTSTriangle alloc] initWithSize:randomSize];
+            break;
+    }
+    
+    // randomize position
+    // making sure the shape will not go out of screen.. too much
+    float minDistanceFromBorder = side * 0.5;
+    float maxDistanceX = screenRect.size.width - (minDistanceFromBorder * 2);
+    float maxDistanceY = screenRect.size.height - (minDistanceFromBorder * 2);
+    
+    float x = arc4random_uniform(maxDistanceX) + minDistanceFromBorder;
+    float y = arc4random_uniform(maxDistanceY) + minDistanceFromBorder;
+    CGPoint randomCenter = CGPointMake(x, y);
+    randomShape.center = randomCenter;
+    
+    return [randomShape autorelease];
+}
+
+
+@end
